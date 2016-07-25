3a3fe447a9d7f13c505135d78ad8d757f1708b7b
makeinjava <makeinjava@gmail.com>
2016-07-25 19:26:56 +0530

    Update binary tree

--- DiameterOfBTree(DFS)/src/main/java/org/learn/Client/App.java
+++ DiameterOfBTree(DFS)/src/main/java/org/learn/Client/App.java
@@ -1,30 +1,53 @@
 package org.learn.Client;
 
-import org.learn.PrepareTree.Node;
 import org.learn.Question.DiameterOfBTree;
+import org.learn.Question.Node;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	public static void main( String[] args )
-    {		
-    	Node root = Node.createNode(100);
-    	root.left        = Node.createNode(50);
-    	root.right       = Node.createNode(150);
-    	//left sub tree
-    	root.left.left  = Node.createNode(25);
-    	root.left.right = Node.createNode(80);
-    	root.left.right.right = Node.createNode(90);
-    	
-    	//right subtree
-    	root.right.left  = Node.createNode(125);
-    	root.right.right = Node.createNode(200);   
-    	
-    	DiameterOfBTree objDiameter = new DiameterOfBTree();
-    	objDiameter.diameterOfBTree(root);
-  		System.out.println("Diameter of Binary Tree is : "+ objDiameter.getDiameter());         
-    }
+public class App {
+	public static void main(String[] args) {
+		// Level 0
+		Node A = Node.createNode(70);
+		// Level 1
+		Node B = Node.createNode(40);
+		Node C = Node.createNode(80);
+		// Level 2
+		Node D = Node.createNode(70);
+		Node E = Node.createNode(40);
+		Node F = Node.createNode(80);
+		Node G = Node.createNode(80);
+		// Level 3
+		Node H = Node.createNode(70);
+		Node I = Node.createNode(40);
+		Node J = Node.createNode(80);
+		// Level 4
+		Node K = Node.createNode(70);
+		Node L = Node.createNode(40);
+		Node M = Node.createNode(80);
+		Node N = Node.createNode(80);
+
+		// Connect level 0 to level 1
+		A.left = B;
+		A.right = C;
+		// Connect level 1 to level 2
+		B.left = D;
+		B.right = E;
+		C.left = F;
+		C.right = G;
+		// Connect level 2 to level 3
+		D.left = H;
+		D.right = I;
+		E.right = J;
+		// Connect level 2 to level 3
+		H.left = K;
+		H.right = L;
+		J.left = M;
+		J.right = N;
+
+		DiameterOfBTree objDiameter = new DiameterOfBTree();
+		System.out.println("1. Diameter at Node K is : " + objDiameter.getDiameter(K));
+		System.out.println("2. Diameter at Node H is : " + objDiameter.getDiameter(H));
+		System.out.println("3. Diameter at Node D is : " + objDiameter.getDiameter(D));
+		System.out.println("4. Diameter at Node B is : " + objDiameter.getDiameter(B));
+		System.out.println("5. Diameter of Binary Tree is : " + objDiameter.getDiameter(A));
+	}
 }
--- DiameterOfBTree(DFS)/src/main/java/org/learn/Question/DiameterOfBTree.java
+++ DiameterOfBTree(DFS)/src/main/java/org/learn/Question/DiameterOfBTree.java
@@ -1,19 +1,23 @@
-
 package org.learn.Question;
-import org.learn.PrepareTree.Node;
 
 public class DiameterOfBTree {
 	int diameter = 0;
-	public int diameterOfBTree(Node root) {
-		if(null == root)
+
+	private int diameterOfBTree(Node root) {
+		if (null == root)
 			return 0;
-		
+
 		int left = diameterOfBTree(root.left);
 		int right = diameterOfBTree(root.right);
-		diameter = Math.max(diameter,left+right+1);
-		return Math.max(left,right) + 1;		
+		diameter = Math.max(diameter, left + right + 1);
+		return Math.max(left, right) + 1;
 	}
-	public int getDiameter() {
-		return this.diameter;
+
+	public int getDiameter(Node root) {
+		diameterOfBTree(root);		
+		int diameterTree = diameter;
+		//reset diameter 
+		diameter = 0;
+		return diameterTree;
 	}
 }
